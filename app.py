ac902154b393710be6808531d9edf44e1e9c4e3c
abdamsab <abdamsabinfo@gmail.com>
2024-07-10 16:14:41 +0100

    add template and stattic

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
